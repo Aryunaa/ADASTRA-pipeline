269d9b32f0678e788bea4bed4e67da9c2fd636ff
Aryuna Ayusheeva <aryuna.ayusheeva.1998@mail.ru>
2021-08-20 00:08:39 +0800

    Add files via upload

--- scripts/snp_calling.sh
+++ scripts/snp_calling.sh
@@ -6,7 +6,7 @@ njobs=$1
 
 if [ ! -f "${dbsnp_vcf_path}.tbi" ]; then
 	echo "Index file for ${dbsnp_vcf_path} not found, indexing.."
-	$Java $JavaParameters  -jar "$GATK" IndexFeatureFile -F ${dbsnp_vcf_path}
+	$Java $JavaParameters  -jar "$GATK" IndexFeatureFile -l ${dbsnp_vcf_path}
 fi
 
 parallel --delay 600 --jobs "$njobs" \
